238ab1303f0cc2113e73964301801c70f645c311
lirazYakubov <88534012+lirazYakubov@users.noreply.github.com>
2021-09-21 21:49:35 +0300

    Add files via upload

---
+++ Liraz_yakubov_app_ex7/app.js
@@ -0,0 +1,35 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const sql=require("./DB/DB.js");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "liraz"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running");
+});
+
+// Create a route for getting all customers
+app.get("/customers", function(req, res) {
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
+        if (err) {
+            console.log("error: ", err);
+            res.status(400).send({ message: "error in getting all customers: " + err });
+            return;
+        }
+        console.log("got all customers...");
+        res.send(mysqlres);
+        return;
+    });
+});
