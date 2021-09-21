64d55498e98539d6f985ccabb224b3a996840b21
lirazYakubov <88534012+lirazYakubov@users.noreply.github.com>
2021-09-21 21:22:31 +0300

    Add files via upload

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,5 +1,35 @@
-var http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.end('Hello World');
-}).listen(8080);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const sql=require("./DB");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "liraz"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running");
+});
+
+// Create a route for getting all customers
+app.get("/customers", function(req, res) {
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
+        if (err) {
+            console.log("error: ", err);
+            res.status(400).send({ message: "error in getting all customers: " + err });
+            return;
+        }
+        console.log("got all customers...");
+        res.send(mysqlres);
+        return;
+    });
+});
